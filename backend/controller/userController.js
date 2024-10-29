55590977ee25f1ca0d5129aceff37f515ddcad65
bapidasdev <das316456@gmail.com>
2024-10-29 08:38:18 +0530

    change frontend link

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,11 +1,13 @@
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+
 import validator from "validator";
 
 
 //login user
 const loginUser = async (req, res) => {
+    
     const { email, password } = req.body;
     try {
         const user = await userModel.findOne({ email });
